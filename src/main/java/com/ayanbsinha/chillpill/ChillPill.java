7ada0f49b5a1cd0bf608dbf1995c5c5a25843d56
Ayan <ayanbsinha@gmail.com>
2023-05-15 21:29:32 -0400

    Initial Commit

--- src/main/java/com/ayanbsinha/chillpill/ChillPill.java
+++ src/main/java/com/ayanbsinha/chillpill/ChillPill.java
@@ -1,19 +1,112 @@
 package com.ayanbsinha.chillpill;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ChillPill {
+    static{
+        alert();
+        System.out.println("***** Take A Chill-Pill *****");
+        System.out.println("# Keeps Your System Engage.");
+        System.out.println("# Helps To Keeps Your Eyes Strain Free.");
+        System.out.println("# Helps To Stay Hydrated.");
+        System.out.println("Launching Chill-Pill......... \n\n");
+    }
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(3);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Thread 1: Runs every 45 seconds
+        executor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                // Code to be executed every 59 seconds
+                try {
+                    ghostFly();
+                } catch (AWTException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 0, 59, TimeUnit.SECONDS);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // Thread 2: Runs every 20 minutes
+        executor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                // Code to be executed every 20 minutes
+                try {
+                    t2020Rule();
+                } catch (LineUnavailableException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 20, 20, TimeUnit.MINUTES);
+
+        // Thread 3: Runs every 1 hour
+        executor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                // Code to be executed every 1 hour
+                alert();
+                System.out.println(getDateTime() +" : Just checking, did you have water in the last hour?");
+            }
+        }, 0, 1, TimeUnit.HOURS);
         }
+        private static String getDateTime(){
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd HH:mm:ss");
+            return currentDateTime.format(formatter);
+        }
+        private static void ghostFly() throws AWTException {
+            Point p = MouseInfo.getPointerInfo().getLocation();
+            Robot robot = new Robot();
+            robot.mouseMove(p.x+1, p.y+1);
+        }
+        private static void alert(){
+            Toolkit.getDefaultToolkit().beep();
+        }
+        private static void t2020Rule() throws LineUnavailableException, InterruptedException {
+            buzz();
+            System.out.println(getDateTime() +" : Relax your eyes by taking a 20-second break following the 20-20-20 rule.");
+            Thread.sleep(20000);
+            buzz();
+            System.out.println(getDateTime() +" : Lets resume to work.");
+        }
+
+        private static void buzz() throws LineUnavailableException {
+            tone(400,500, 0.2);
+        }
+    private static float SAMPLE_RATE = 8000f;
+    private static void tone(int hz, int msecs, double vol)
+            throws LineUnavailableException
+    {
+        byte[] buf = new byte[1];
+        AudioFormat af =
+                new AudioFormat(
+                        SAMPLE_RATE, // sampleRate
+                        8,           // sampleSizeInBits
+                        1,           // channels
+                        true,        // signed
+                        false);      // bigEndian
+        SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+        sdl.open(af);
+        sdl.start();
+        for (int i=0; i < msecs*8; i++) {
+            double angle = i / (SAMPLE_RATE / hz) * 2.0 * Math.PI;
+            buf[0] = (byte)(Math.sin(angle) * 127.0 * vol);
+            sdl.write(buf,0,1);
+        }
+        sdl.drain();
+        sdl.stop();
+        sdl.close();
     }
 }
--- src/main/java/com/ayanbsinha/chillpill/Sound.java
+++
@@ -1,2 +0,0 @@
-package com.ayanbsinha.chillpill;public class Sound {
-}
